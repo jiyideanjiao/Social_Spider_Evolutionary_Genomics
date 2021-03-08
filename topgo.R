3fa7b1bd717ea6d0a73aec10519020bfe81080aa
Chao Tong <33243479+jiyideanjiao@users.noreply.github.com>
2021-03-08 01:05:12 -0500

    Create topgo.R

---
+++ topgo.R
@@ -0,0 +1,26 @@
+library(topGO)
+setwd("path/")
+
+gene.BPGO.list=read.csv("{go_annotation.csv}")
+gene.BPGO=strsplit(gene.BPGO.list$go_id,",")
+names(gene.BPGO)=gene.BPGO.list$OGG_ID
+
+geneNames=names(gene.BPGO)
+gene.list.gT=read.csv("{gene.csv}")
+sig.list.gT=subset(gene.list.gT)$OGG_ID
+
+GOfunc <- function(myInterestingGenes){
+  geneList <- factor(as.integer(geneNames %in% myInterestingGenes))
+  names(geneList) <- geneNames
+  GOdata <- new("topGOdata", ontology = "BP", allGenes = geneList,
+                annot = annFUN.gene2GO, gene2GO = gene.BPGO, nodeSize=10)
+  resultFisher <- runTest(GOdata, algorithm = "classic", statistic = "fisher")
+  tab <- GenTable(GOdata,resultFisher, topNodes = 500,numChar=500)
+  sig.tab=subset(tab,as.numeric(result1)<0.05)
+  return(sig.tab)
+}
+
+GOfunc(sig.list.gT)
+
+gene_result=GOfunc(sig.list.gT)
+write.csv(gene_result, file = "{output_enriched_GO.csv}")
