32f4c0b88bd333346c336669be0df097c1bf18d6
Chao Tong <33243479+jiyideanjiao@users.noreply.github.com>
2021-03-07 23:45:55 -0500

    Add files via upload

---
+++ FastaHeaderOGG.py
@@ -0,0 +1,51 @@
+#!/usr/local/bin/python2.7
+#File takes in a fasta file containing protein sequences from many species
+#The protein sequences are labeled by their unique OrthoDB8 key. 
+#The purpose is to replace the key with >Ortholog|species|gene name
+from Bio import SeqIO
+import sys, getopt, re
+
+def InOut(argv):
+	inputfile=''
+	outputfile = ''
+	try:
+	        opts, args = getopt.getopt(argv,"hi:o:s:",["ifile=","ofile=","sfile="])
+	except getopt.GetoptError:
+	        print 'FastaHeaderOGG.py -i <inputfile> -o <outputfile> -s <seqfile>'
+	        sys.exit(2)
+	for opt, arg in opts:
+	        if opt in ("-i","--ifile"):
+	                inputfile = arg
+	        elif opt in ("-o","--ofile"):
+	                outputfile = arg   
+	        elif opt in ("-s","--sfile"):
+	                seqfile = arg                    
+	return inputfile,outputfile,seqfile
+
+
+def main(argv):
+	inputfile,outputfile,seqfile = InOut(argv)
+	OGGdict = {} #create dictionary to store genes with the new ID names
+	with open(seqfile,'r') as f:
+		f.readline() #skip header line
+		for line in f:
+			l = line[:-1]
+			#The line below parses the line from the OrthoDB file and returns the new header
+			newl = re.sub(r'.*\t([A-Z0-9]+)\t([A-Za-z]*).*\t([0-9]+:[0-9a-zA-Z]+).*',r'\g<1>|\g<2>|\g<3>',l)
+			gene = re.sub(r'.*\|.*\|([0-9]+:[0-9a-zA-Z]+).*',r'\g<1>',newl)
+			OGGdict[gene]=newl
+	newfasta= open(outputfile, 'w')
+	with open(inputfile,'r') as f:
+		for line in f:
+			if line.startswith('>'):
+				gene = re.findall('>[0-9]+:[0-9a-zA-Z]+',line) ###Fix to find the gene (number code)
+				gene = gene[0][1:] #convert from list, strip ">"
+				newfasta.write(">"+OGGdict[gene]+"\n")
+			else:
+				newfasta.write(line)
+	newfasta.close()
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:]) 
+
---
+++ OGGalign.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python2.7
+##File takes in the name of an OGG from OrthoDB7
+##Finds all sequences for all ant species, uses MAFFT, PRANK, and HMMER to make markov models of OGGs
+
+import numpy as np, sys, getopt, pandas as pd
+from subprocess import call
+import subprocess
+from joblib import Parallel, delayed
+import re
+
+def InOut(argv):
+	inputfile=''
+	outputfile = ''
+	try:
+	        opts, args = getopt.getopt(argv,"h:t:s:",["tfile=","sfile="])
+	except getopt.GetoptError:
+	        print 'OGGalign.py -t <threads> -s <seqfile>'
+	        sys.exit(2)
+	for opt, arg in opts:
+	        if opt in ("-t","--tfile"):
+	                num_cores = arg 
+	        elif opt in ("-s","--sfile"):
+	                seqfile = arg                    
+	return num_cores,seqfile
+
+def alignGene(OGG,data):
+	call(["prank","-d="+OGG+".fa","-o="+OGG])
+	call(["mv",OGG+".best.fas",OGG+".fas"])
+	call(["convbioseq","stockholm",OGG+".fas"])
+	call(["hmmbuild",OGG+".hmm",OGG+".sth"])
+	call(["rm",OGG+".fa",OGG+".sth",OGG+".fas"])
+	return
+
+def main(argv):
+	num_cores,seqfile = InOut(argv)
+	df = pd.read_csv(seqfile,sep="\t")
+	data = np.array(df)
+	OGGS = np.unique(data[:,1])
+	for OGG in OGGS:
+		Parallel(n_jobs=int(num_cores))(delayed(alignGene)(OGG,data)for OGG in OGGS)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
