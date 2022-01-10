997136d3f571549abbdc9d990b8dd0ae5567ad53
Chao Tong <33243479+jiyideanjiao@users.noreply.github.com>
2022-01-09 21:28:22 -0500

    Create gene_model_augustus.sh

---
+++ gene_model_augustus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -ve
+
+#SBATCH -p compute # partition (queue)
+#SBATCH --export=ALL
+#SBATCH --cpus-per-task=1
+
+augustus --species=parasteatoda --codingseq=on final.fasta > gene_models_augustus.gff
