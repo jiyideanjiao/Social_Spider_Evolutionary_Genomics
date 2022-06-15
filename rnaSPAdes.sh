4878e63adc8677a56a69861f71020d1f6ef5766f
Chao Tong <33243479+jiyideanjiao@users.noreply.github.com>
2022-06-15 15:23:54 -0400

    Update rnaSPAdes.sh

--- rnaSPAdes.sh
+++ rnaSPAdes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ve
 #SBATCH -p compute # partition (queue)
 #SBATCH --export=ALL
-#SBATCH --cpus-per-task=20
+#SBATCH -n 20
 
 ~/spades.py --rna -1 trimmed_1P.fastq -2 trimmed_2P.fastq -t 20 -o output_dir
