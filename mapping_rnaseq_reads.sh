79cd2f189d838d16ad446c8afa93874718f81786
Chao Tong <33243479+jiyideanjiao@users.noreply.github.com>
2022-06-15 15:17:53 -0400

    Update mapping_rnaseq_reads.sh

--- mapping_rnaseq_reads.sh
+++ mapping_rnaseq_reads.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p compute # partition (queue)
 #SBATCH --export=ALL
-#SBATCH -n 15
+#SBATCH -n {number}
 
 hisat2-build spider.fasta spider_hisat
 hisat2 -x spider_hisat -1 ../fastq/R1.fastq -2 ../fastq/R2.fastq -k 3 -p 20 --pen-noncansplice 1000000 -S input.sam
