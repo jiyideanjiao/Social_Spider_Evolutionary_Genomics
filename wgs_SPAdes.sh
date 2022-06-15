2f3fe14c058fb43f2eb772ced7788408779e9b95
Chao Tong <33243479+jiyideanjiao@users.noreply.github.com>
2022-06-15 15:14:11 -0400

    Update wgs_SPAdes.sh

--- wgs_SPAdes.sh
+++ wgs_SPAdes.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ve
 #SBATCH -p compute # partition (queue)
 #SBATCH --export=ALL
-#SBATCH -n 20
+#SBATCH -n {number}
 
 ~/spades.py \
 --pe1-1 trimmed_1P.fastq \
 --pe1-2 trimmed_2P.fastq \
 -k 13,15,17,19,23,33,43,53,63,73,83,93,103 \
--t 20 \
+-t {number} \
 -o output_genome
