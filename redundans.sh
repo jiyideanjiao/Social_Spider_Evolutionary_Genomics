59e0589dfd545ab84c481bde6fa713b72ac1806b
Chao Tong <33243479+jiyideanjiao@users.noreply.github.com>
2022-06-15 15:23:37 -0400

    Update redundans.sh

--- redundans.sh
+++ redundans.sh
@@ -2,6 +2,6 @@
 
 #SBATCH -p compute # partition (queue)
 #SBATCH --export=ALL
-#SBATCH --cpus-per-task=15
+#SBATCH -n 15
 
 ~/redundans.py -v -i *.fastq -f contigs.fasta -o ./output -t 15 --log ./log
