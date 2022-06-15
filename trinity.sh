1041189bbf7ab78cdff41d8252171d5532e67c28
Chao Tong <33243479+jiyideanjiao@users.noreply.github.com>
2022-06-15 15:14:37 -0400

    Update trinity.sh

--- trinity.sh
+++ trinity.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p compute # partition (queue)
 #SBATCH --export=ALL
-#SBATCH --cpus-per-task=20
+#SBATCH -n {number}
 
 Trinity \
 --trimmomatic \
@@ -10,7 +10,7 @@ Trinity \
 --max_memory 20G \
 --left left.fq \
 --right right.fq \
---CPU 20 \
+--CPU {number} \
 --output trinity_output \
 --no_bowtie \
 --quality_trimming_params "SLIDINGWINDOW:4:20 LEADING:10 TRAILING:10 MINLEN:70"  \
